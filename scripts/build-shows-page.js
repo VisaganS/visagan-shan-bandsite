e87011b935ec2c534154e7195c63212c109cbcea
Visagan Shan <visagan.shan@gmail.com>
2023-05-11 02:30:04 -0400

    minor styling changes according to feedback

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,114 +1,110 @@
 let arrShows = [
-    {
-        date: 'Mon Sept 06 2021',
-        venue: 'Ronald Lane',
-        location: 'San Francisco, CA'
-    },
-    {
-        date: 'Tues Sept 21 2021',
-        venue: 'Pier 3 East',
-        location: 'San Francisco, CA'
-    },
-    {
-        date: 'Fri Oct 15 2021',
-        venue: 'View Lounge',
-        location: 'San Francisco, CA'
-    },
-    {
-        date: 'Sat Nov 06 2021',
-        venue: 'Hyatt Agency',
-        location: 'San Francisco, CA'
-    },
-    {
-        date: 'Fri Nov 26 2021',
-        venue: 'Moscow Center',
-        location: 'San Francisco, CA'
-    },
-    {
-        date: 'Wed Dec 15 2021',
-        venue: 'Press Club',
-        location: 'San Francisco, CA'
-    }
+  {
+    date: "Mon Sept 06 2021",
+    venue: "Ronald Lane",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Tues Sept 21 2021",
+    venue: "Pier 3 East",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Fri Oct 15 2021",
+    venue: "View Lounge",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Sat Nov 06 2021",
+    venue: "Hyatt Agency",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Fri Nov 26 2021",
+    venue: "Moscow Center",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Wed Dec 15 2021",
+    venue: "Press Club",
+    location: "San Francisco, CA",
+  },
 ];
 
+function displayShows() {
+  let showsDisplay = document.querySelector(".shows__display");
 
-function displayShows(){
-    let showsDisplay = document.querySelector(".shows__display");
+  let showsHeaderTopEl = document.createElement("div");
+  showsHeaderTopEl.classList.add("shows__header-top");
 
-    let showsHeaderTopEl = document.createElement('div');
-    showsHeaderTopEl.classList.add('shows__header-top');
+  let showsHeadingTopEl = document.createElement("div");
+  showsHeadingTopEl.classList.add("shows__heading--top");
+  showsHeadingTopEl.innerHTML = "DATE";
+  showsHeaderTopEl.appendChild(showsHeadingTopEl);
 
-    let showsHeadingTopEl = document.createElement('div');
-    showsHeadingTopEl.classList.add('shows__heading--top');
-    showsHeadingTopEl.innerHTML = "DATE";
-    showsHeaderTopEl.appendChild(showsHeadingTopEl);
+  showsHeadingTopEl = document.createElement("div");
+  showsHeadingTopEl.classList.add("shows__heading--top");
+  showsHeadingTopEl.innerHTML = "VENUE";
+  showsHeaderTopEl.appendChild(showsHeadingTopEl);
 
-    showsHeadingTopEl = document.createElement('div');
-    showsHeadingTopEl.classList.add('shows__heading--top');
-    showsHeadingTopEl.innerHTML = "VENUE";
-    showsHeaderTopEl.appendChild(showsHeadingTopEl);
+  showsHeadingTopEl = document.createElement("div");
+  showsHeadingTopEl.classList.add("shows__heading--top");
+  showsHeadingTopEl.innerHTML = "LOCATION";
+  showsHeaderTopEl.appendChild(showsHeadingTopEl);
 
-    showsHeadingTopEl = document.createElement('div');
-    showsHeadingTopEl.classList.add('shows__heading--top');
-    showsHeadingTopEl.innerHTML = "LOCATION";
-    showsHeaderTopEl.appendChild(showsHeadingTopEl);
+  showsDisplay.appendChild(showsHeaderTopEl);
 
-    showsDisplay.appendChild(showsHeaderTopEl);
+  arrShows.forEach((element) => {
+    let showsItemEl = document.createElement("div");
+    showsItemEl.classList.add("shows__item");
 
-    arrShows.forEach(element =>{
-    let showsItemEl = document.createElement('div');
-    showsItemEl.classList.add('shows__item');
-
-    let showsHeadingEl = document.createElement('div');
-    showsHeadingEl.classList.add('shows__heading');
+    let showsHeadingEl = document.createElement("div");
+    showsHeadingEl.classList.add("shows__heading");
     showsHeadingEl.innerHTML = "DATE";
     showsItemEl.appendChild(showsHeadingEl);
 
-    let showsInfoEl = document.createElement('div');
-    showsInfoEl.classList.add('shows__info--bold');
+    let showsInfoEl = document.createElement("div");
+    showsInfoEl.classList.add("shows__info--bold");
     showsInfoEl.innerText = element.date;
     showsItemEl.appendChild(showsInfoEl);
 
-    showsHeadingEl = document.createElement('div');
-    showsHeadingEl.classList.add('shows__heading');
+    showsHeadingEl = document.createElement("div");
+    showsHeadingEl.classList.add("shows__heading");
     showsHeadingEl.innerHTML = "DATE";
     showsItemEl.appendChild(showsHeadingEl);
 
-
-    showsInfoEl = document.createElement('div');
-    showsInfoEl.classList.add('shows__info');
+    showsInfoEl = document.createElement("div");
+    showsInfoEl.classList.add("shows__info");
     showsInfoEl.innerText = element.venue;
     showsItemEl.appendChild(showsInfoEl);
 
-    showsHeadingEl = document.createElement('div');
-    showsHeadingEl.classList.add('shows__heading');
+    showsHeadingEl = document.createElement("div");
+    showsHeadingEl.classList.add("shows__heading");
     showsHeadingEl.innerHTML = "LOCATION";
     showsItemEl.appendChild(showsHeadingEl);
 
-    showsInfoEl = document.createElement('div');
-    showsInfoEl.classList.add('shows__info');
+    showsInfoEl = document.createElement("div");
+    showsInfoEl.classList.add("shows__info");
     showsInfoEl.innerText = element.location;
     showsItemEl.appendChild(showsInfoEl);
-    
-    let showsButtonEl = document.createElement('button');
-    showsButtonEl.classList.add('shows__button');
+
+    let showsButtonEl = document.createElement("button");
+    showsButtonEl.classList.add("shows__button");
     showsButtonEl.innerText = "BUY TICKETS";
     showsItemEl.appendChild(showsButtonEl);
 
     showsDisplay.appendChild(showsItemEl);
-    });
+  });
 }
 
 displayShows();
 
 let showsClick = document.querySelectorAll(".shows__item");
-for(let i = 0; i < showsClick.length; i++){
-    showsClick[i].addEventListener('click', ()=>{
-        for(let x = 0; x < showsClick.length; x++){
-            showsClick[x].style.backgroundColor = "white";
-        }
-        showsClick[i].style.backgroundColor = '#E1E1E1';
-    });
+for (let i = 0; i < showsClick.length; i++) {
+  showsClick[i].addEventListener("click", () => {
+    for (let x = 0; x < showsClick.length; x++) {
+      showsClick[x].classList.remove("shows__item--active");
+    }
+    showsClick[i].classList.add("shows__item--active");
+  });
 }
-
-
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,97 +1,109 @@
+const apiKey = "f58c786c-9bdb-45c7-b12d-b6d2098aafd1";
+
+axios.get(`https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`)
+.then((result) => {
+console.log(result.data);
+});
 arrComments = [
-    {name: 'Connor Walton',
-     date: '02/17/2021',
-     image:{
-         url: "",
-         alt: "",
-     },
-     comment: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work ' + 
-     'deserves reverence. Let us appreciate this for what it is and what it contains.'
+  {
+    name: "Connor Walton",
+    date: "02/17/2021",
+    image: {
+      url: "",
+      alt: "",
     },
-    {name: 'Emilie Beach',
-     date: '01/09/2021',
-     image:{
-         url: "",
-         alt: "",
-     },
-     comment: 'I feel blessed to have seen them in person. What a show! They were just perfection.' +
-     ' If there was one day of my life I could relive, this would be it. What an incredible day.'
+    comment:
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work " +
+      "deserves reverence. Let us appreciate this for what it is and what it contains.",
+  },
+  {
+    name: "Emilie Beach",
+    date: "01/09/2021",
+    image: {
+      url: "",
+      alt: "",
     },
-    {name: 'Miles Acosta',
-     date: '12/20/2020',
-     image:{
-         url: "",
-         alt: "",
-     },
-     comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps."
-      + " Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
-     
-    }
+    comment:
+      "I feel blessed to have seen them in person. What a show! They were just perfection." +
+      " If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    name: "Miles Acosta",
+    date: "12/20/2020",
+    image: {
+      url: "",
+      alt: "",
+    },
+    comment:
+      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps." +
+      " Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  },
 ];
 
-function displayComments(){
+function displayComments() {
+  let commentsDisplay = document.querySelector(".comments__display");
+  commentsDisplay.innerHTML = "";
 
-    let commentsDisplay = document.querySelector('.comments__display');
-    commentsDisplay.innerHTML = "";
-    
-    arrComments.forEach(element =>{
-        let itemEl = document.createElement("div");
-        itemEl.classList.add('item');
-    
-        let itemImgContainerEl = document.createElement("div");
-        itemImgContainerEl.classList.add('item__image-container');
-        itemEl.appendChild(itemImgContainerEl);
+  arrComments.forEach((element) => {
+    let itemEl = document.createElement("div");
+    itemEl.classList.add("item");
 
-        if(element.image.url){
-        let itemImageEl = document.createElement('img');
-        itemImageEl.setAttribute('src', element.image.url);
-        itemImageEl.alt = element.image.alt;
-        itemImageEl.classList.add('card__image');
-        itemImgContainerEl.appendChild(itemImageEl);
-        }
-    
-        let itemTextEl = document.createElement("div");
-        itemTextEl.classList.add('item__text');
-        itemEl.appendChild(itemTextEl);
-    
-        let itemHeaderEl = document.createElement("div");
-        itemHeaderEl.classList.add('item__header');
-        itemTextEl.appendChild(itemHeaderEl);
-    
-        let itemNameEl = document.createElement("div");
-        itemNameEl.classList.add('item__name');
-        itemNameEl.innerText = element.name;
-        itemHeaderEl.appendChild(itemNameEl);
-    
-        let itemDateEl = document.createElement("div");
-        itemDateEl.classList.add('item__date');
-        itemDateEl.innerText = element.date;
-        itemHeaderEl.appendChild(itemDateEl);
-    
-        let itemBodyEl = document.createElement("div");
-        itemBodyEl.classList.add('item__body');
-        itemBodyEl.innerText = element.comment;
-        itemTextEl.appendChild(itemBodyEl);
-    
-        commentsDisplay.appendChild(itemEl);
-    });
+    let itemImgContainerEl = document.createElement("div");
+    itemImgContainerEl.classList.add("item__image-container");
+    itemEl.appendChild(itemImgContainerEl);
+
+    if (element.image.url) {
+      let itemImageEl = document.createElement("img");
+      itemImageEl.setAttribute("src", element.image.url);
+      itemImageEl.alt = element.image.alt;
+      itemImageEl.classList.add("item__image");
+      itemImgContainerEl.appendChild(itemImageEl);
     }
 
- displayComments(); 
+    let itemTextEl = document.createElement("div");
+    itemTextEl.classList.add("item__text");
+    itemEl.appendChild(itemTextEl);
 
-const form = document.querySelector('.form-inputs');
-form.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    commentObj = {
-        name: e.target.name.value,
-        date: new Date().toLocaleDateString(),
-        comment: e.target.comment.value,
-        image: {
-            url: "../assets/Images/Mohan-muruge.jpg",
-            alt: "profile-pic"
-        }
-    };
-    arrComments.push(commentObj);
-    displayComments();
+    let itemHeaderEl = document.createElement("div");
+    itemHeaderEl.classList.add("item__header");
+    itemTextEl.appendChild(itemHeaderEl);
+
+    let itemNameEl = document.createElement("div");
+    itemNameEl.classList.add("item__name");
+    itemNameEl.innerText = element.name;
+    itemHeaderEl.appendChild(itemNameEl);
+
+    let itemDateEl = document.createElement("div");
+    itemDateEl.classList.add("item__date");
+    itemDateEl.innerText = element.date;
+    itemHeaderEl.appendChild(itemDateEl);
+
+    let itemBodyEl = document.createElement("div");
+    itemBodyEl.classList.add("item__body");
+    itemBodyEl.innerText = element.comment;
+    itemTextEl.appendChild(itemBodyEl);
+
+    commentsDisplay.appendChild(itemEl);
+  });
+}
+
+displayComments();
+
+const form = document.querySelector(".form-inputs");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  commentObj = {
+    name: e.target.name.value,
+    date: new Date().toLocaleDateString(),
+    comment: e.target.comment.value,
+    image: {
+      url: "../assets/Images/Mohan-muruge.jpg",
+      alt: "profile-pic",
+    }
+  };
+  e.target.name.value = "";
+  e.target.comment.value = "";
+  arrComments.unshift(commentObj);
+  displayComments();
 });
 
