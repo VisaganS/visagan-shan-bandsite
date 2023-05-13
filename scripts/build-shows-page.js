d86ea751fa7eb623c62aff207a24effdfca665b1
Visagan Shan <visagan.shan@gmail.com>
2023-05-13 02:56:08 -0400

    api working on shows, comments section partially refactored

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,105 +1,89 @@
-let arrShows = [
-  {
-    date: "Mon Sept 06 2021",
-    venue: "Ronald Lane",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Tues Sept 21 2021",
-    venue: "Pier 3 East",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Fri Oct 15 2021",
-    venue: "View Lounge",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Sat Nov 06 2021",
-    venue: "Hyatt Agency",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Fri Nov 26 2021",
-    venue: "Moscow Center",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Wed Dec 15 2021",
-    venue: "Press Club",
-    location: "San Francisco, CA",
-  },
-];
+const apiKey = "25636357-21e4-4272-8dfc-d14667c810d9";
 
-function displayShows() {
-  let showsDisplay = document.querySelector(".shows__display");
+axios.get(`https://project-1-api.herokuapp.com/showdates?api_key=${apiKey}`)
+.then(result => {
+  const arrShows = result.data;
+  console.log(arrShows);
 
-  let showsHeaderTopEl = document.createElement("div");
-  showsHeaderTopEl.classList.add("shows__header-top");
+  function displayShows() {
+    let showsDisplay = document.querySelector(".shows__display");
+  
+    let showsHeaderTopEl = document.createElement("div");
+    showsHeaderTopEl.classList.add("shows__header-top");
+  
+    let showsHeadingTopEl = document.createElement("div");
+    showsHeadingTopEl.classList.add("shows__heading--top");
+    showsHeadingTopEl.innerHTML = "DATE";
+    showsHeaderTopEl.appendChild(showsHeadingTopEl);
+  
+    showsHeadingTopEl = document.createElement("div");
+    showsHeadingTopEl.classList.add("shows__heading--top");
+    showsHeadingTopEl.innerHTML = "VENUE";
+    showsHeaderTopEl.appendChild(showsHeadingTopEl);
+  
+    showsHeadingTopEl = document.createElement("div");
+    showsHeadingTopEl.classList.add("shows__heading--top");
+    showsHeadingTopEl.innerHTML = "LOCATION";
+    showsHeaderTopEl.appendChild(showsHeadingTopEl);
+  
+    showsDisplay.appendChild(showsHeaderTopEl);
+  
+    arrShows.forEach((element) => {
 
-  let showsHeadingTopEl = document.createElement("div");
-  showsHeadingTopEl.classList.add("shows__heading--top");
-  showsHeadingTopEl.innerHTML = "DATE";
-  showsHeaderTopEl.appendChild(showsHeadingTopEl);
+      let formattedDate = new Date(element.date).toLocaleString("en-US", {
+        weekday: "short",
+        year: "numeric",
+        month: "short",
+        day: "numeric"
+      });
 
-  showsHeadingTopEl = document.createElement("div");
-  showsHeadingTopEl.classList.add("shows__heading--top");
-  showsHeadingTopEl.innerHTML = "VENUE";
-  showsHeaderTopEl.appendChild(showsHeadingTopEl);
+      formattedDate = formattedDate.split(',');
+      formattedDate = formattedDate.join(' ');
 
-  showsHeadingTopEl = document.createElement("div");
-  showsHeadingTopEl.classList.add("shows__heading--top");
-  showsHeadingTopEl.innerHTML = "LOCATION";
-  showsHeaderTopEl.appendChild(showsHeadingTopEl);
+      let showsItemEl = document.createElement("div");
+      showsItemEl.classList.add("shows__item");
+  
+      let showsHeadingEl = document.createElement("div");
+      showsHeadingEl.classList.add("shows__heading");
+      showsHeadingEl.innerHTML = "DATE";
+      showsItemEl.appendChild(showsHeadingEl);
+  
+      let showsInfoEl = document.createElement("div");
+      showsInfoEl.classList.add("shows__info--bold");
+      showsInfoEl.innerText = formattedDate;
+      showsItemEl.appendChild(showsInfoEl);
+  
+      showsHeadingEl = document.createElement("div");
+      showsHeadingEl.classList.add("shows__heading");
+      showsHeadingEl.innerHTML = "DATE";
+      showsItemEl.appendChild(showsHeadingEl);
+  
+      showsInfoEl = document.createElement("div");
+      showsInfoEl.classList.add("shows__info");
+      showsInfoEl.innerText = element.place;
+      showsItemEl.appendChild(showsInfoEl);
+  
+      showsHeadingEl = document.createElement("div");
+      showsHeadingEl.classList.add("shows__heading");
+      showsHeadingEl.innerHTML = "LOCATION";
+      showsItemEl.appendChild(showsHeadingEl);
+  
+      showsInfoEl = document.createElement("div");
+      showsInfoEl.classList.add("shows__info");
+      showsInfoEl.innerText = element.location;
+      showsItemEl.appendChild(showsInfoEl);
+  
+      let showsButtonEl = document.createElement("button");
+      showsButtonEl.classList.add("shows__button");
+      showsButtonEl.innerText = "BUY TICKETS";
+      showsItemEl.appendChild(showsButtonEl);
+  
+      showsDisplay.appendChild(showsItemEl);
+    });
+  }
+  displayShows();
 
-  showsDisplay.appendChild(showsHeaderTopEl);
-
-  arrShows.forEach((element) => {
-    let showsItemEl = document.createElement("div");
-    showsItemEl.classList.add("shows__item");
-
-    let showsHeadingEl = document.createElement("div");
-    showsHeadingEl.classList.add("shows__heading");
-    showsHeadingEl.innerHTML = "DATE";
-    showsItemEl.appendChild(showsHeadingEl);
-
-    let showsInfoEl = document.createElement("div");
-    showsInfoEl.classList.add("shows__info--bold");
-    showsInfoEl.innerText = element.date;
-    showsItemEl.appendChild(showsInfoEl);
-
-    showsHeadingEl = document.createElement("div");
-    showsHeadingEl.classList.add("shows__heading");
-    showsHeadingEl.innerHTML = "DATE";
-    showsItemEl.appendChild(showsHeadingEl);
-
-    showsInfoEl = document.createElement("div");
-    showsInfoEl.classList.add("shows__info");
-    showsInfoEl.innerText = element.venue;
-    showsItemEl.appendChild(showsInfoEl);
-
-    showsHeadingEl = document.createElement("div");
-    showsHeadingEl.classList.add("shows__heading");
-    showsHeadingEl.innerHTML = "LOCATION";
-    showsItemEl.appendChild(showsHeadingEl);
-
-    showsInfoEl = document.createElement("div");
-    showsInfoEl.classList.add("shows__info");
-    showsInfoEl.innerText = element.location;
-    showsItemEl.appendChild(showsInfoEl);
-
-    let showsButtonEl = document.createElement("button");
-    showsButtonEl.classList.add("shows__button");
-    showsButtonEl.innerText = "BUY TICKETS";
-    showsItemEl.appendChild(showsButtonEl);
-
-    showsDisplay.appendChild(showsItemEl);
-  });
-}
-
-displayShows();
-
-let showsClick = document.querySelectorAll(".shows__item");
+  let showsClick = document.querySelectorAll(".shows__item");
 for (let i = 0; i < showsClick.length; i++) {
   showsClick[i].addEventListener("click", () => {
     for (let x = 0; x < showsClick.length; x++) {
@@ -108,3 +92,38 @@ for (let i = 0; i < showsClick.length; i++) {
     showsClick[i].classList.add("shows__item--active");
   });
 }
+
+})
+
+// let arrShows = [
+//   {
+//     date: "Mon Sept 06 2021",
+//     venue: "Ronald Lane",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: "Tues Sept 21 2021",
+//     venue: "Pier 3 East",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: "Fri Oct 15 2021",
+//     venue: "View Lounge",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: "Sat Nov 06 2021",
+//     venue: "Hyatt Agency",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: "Fri Nov 26 2021",
+//     venue: "Moscow Center",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: "Wed Dec 15 2021",
+//     venue: "Press Club",
+//     location: "San Francisco, CA",
+//   },
+// ];
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,64 +1,28 @@
 const apiKey = "f58c786c-9bdb-45c7-b12d-b6d2098aafd1";
 
 axios.get(`https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`)
-.then((result) => {
-console.log(result.data);
-});
-arrComments = [
-  {
-    name: "Connor Walton",
-    date: "02/17/2021",
-    image: {
-      url: "",
-      alt: "",
-    },
-    comment:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work " +
-      "deserves reverence. Let us appreciate this for what it is and what it contains.",
-  },
-  {
-    name: "Emilie Beach",
-    date: "01/09/2021",
-    image: {
-      url: "",
-      alt: "",
-    },
-    comment:
-      "I feel blessed to have seen them in person. What a show! They were just perfection." +
-      " If there was one day of my life I could relive, this would be it. What an incredible day.",
-  },
-  {
-    name: "Miles Acosta",
-    date: "12/20/2020",
-    image: {
-      url: "",
-      alt: "",
-    },
-    comment:
-      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps." +
-      " Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-  },
-];
 
-function displayComments() {
+.then((result) => {
+  let arrComments = result.data;
+  console.log(arrComments);
+
   let commentsDisplay = document.querySelector(".comments__display");
   commentsDisplay.innerHTML = "";
 
   arrComments.forEach((element) => {
+
+    const formattedDate = new Date(element.timestamp).toLocaleString('en-US', {
+      month: 'numeric',
+      day: 'numeric',
+      year: 'numeric'
+    });
+
     let itemEl = document.createElement("div");
     itemEl.classList.add("item");
 
     let itemImgContainerEl = document.createElement("div");
     itemImgContainerEl.classList.add("item__image-container");
     itemEl.appendChild(itemImgContainerEl);
-
-    if (element.image.url) {
-      let itemImageEl = document.createElement("img");
-      itemImageEl.setAttribute("src", element.image.url);
-      itemImageEl.alt = element.image.alt;
-      itemImageEl.classList.add("item__image");
-      itemImgContainerEl.appendChild(itemImageEl);
-    }
 
     let itemTextEl = document.createElement("div");
     itemTextEl.classList.add("item__text");
@@ -75,7 +39,7 @@ function displayComments() {
 
     let itemDateEl = document.createElement("div");
     itemDateEl.classList.add("item__date");
-    itemDateEl.innerText = element.date;
+    itemDateEl.innerText = formattedDate;
     itemHeaderEl.appendChild(itemDateEl);
 
     let itemBodyEl = document.createElement("div");
@@ -85,25 +49,128 @@ function displayComments() {
 
     commentsDisplay.appendChild(itemEl);
   });
-}
+})
+.catch((error) => {
+  console.error(error);
+})
 
-displayComments();
+// arrComments = [
+//   {
+//     name: "Connor Walton",
+//     date: "02/17/2021",
+//     image: {
+//       url: "",
+//       alt: "",
+//     },
+//     comment:
+//       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work " +
+//       "deserves reverence. Let us appreciate this for what it is and what it contains.",
+//   },
+//   {
+//     name: "Emilie Beach",
+//     date: "01/09/2021",
+//     image: {
+//       url: "",
+//       alt: "",
+//     },
+//     comment:
+//       "I feel blessed to have seen them in person. What a show! They were just perfection." +
+//       " If there was one day of my life I could relive, this would be it. What an incredible day.",
+//   },
+//   {
+//     name: "Miles Acosta",
+//     date: "12/20/2020",
+//     image: {
+//       url: "",
+//       alt: "",
+//     },
+//     comment:
+//       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps." +
+//       " Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+//   },
+// ];
+
+// function displayComments() {
+//   let commentsDisplay = document.querySelector(".comments__display");
+//   commentsDisplay.innerHTML = "";
+
+//   arrComments.forEach((element) => {
+//     let itemEl = document.createElement("div");
+//     itemEl.classList.add("item");
+
+//     let itemImgContainerEl = document.createElement("div");
+//     itemImgContainerEl.classList.add("item__image-container");
+//     itemEl.appendChild(itemImgContainerEl);
+
+//     if (element.image.url) {
+//       let itemImageEl = document.createElement("img");
+//       itemImageEl.setAttribute("src", element.image.url);
+//       itemImageEl.alt = element.image.alt;
+//       itemImageEl.classList.add("item__image");
+//       itemImgContainerEl.appendChild(itemImageEl);
+//     }
+
+//     let itemTextEl = document.createElement("div");
+//     itemTextEl.classList.add("item__text");
+//     itemEl.appendChild(itemTextEl);
+
+//     let itemHeaderEl = document.createElement("div");
+//     itemHeaderEl.classList.add("item__header");
+//     itemTextEl.appendChild(itemHeaderEl);
+
+//     let itemNameEl = document.createElement("div");
+//     itemNameEl.classList.add("item__name");
+//     itemNameEl.innerText = element.name;
+//     itemHeaderEl.appendChild(itemNameEl);
+
+//     let itemDateEl = document.createElement("div");
+//     itemDateEl.classList.add("item__date");
+//     itemDateEl.innerText = element.date;
+//     itemHeaderEl.appendChild(itemDateEl);
+
+//     let itemBodyEl = document.createElement("div");
+//     itemBodyEl.classList.add("item__body");
+//     itemBodyEl.innerText = element.comment;
+//     itemTextEl.appendChild(itemBodyEl);
+
+//     commentsDisplay.appendChild(itemEl);
+//   });
+// }
+
+// displayComments();
 
 const form = document.querySelector(".form-inputs");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   commentObj = {
     name: e.target.name.value,
-    date: new Date().toLocaleDateString(),
+    // date: new Date().toLocaleDateString(),
     comment: e.target.comment.value,
-    image: {
-      url: "../assets/Images/Mohan-muruge.jpg",
-      alt: "profile-pic",
-    }
+    // image: {
+    //   url: "../assets/Images/Mohan-muruge.jpg",
+    //   alt: "profile-pic",
+    // }
   };
   e.target.name.value = "";
   e.target.comment.value = "";
-  arrComments.unshift(commentObj);
-  displayComments();
+
+  const headers = {
+    'Content-Type': 'application/json'
+  }
+
+  axios.post(`https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`,commentObj, {
+    headers: headers
+  })
+  .then((result) => {
+    result.data.unshift(commentObj);
+    console.log(result.data);
+    return axios.get(`https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`);
+  })
+  .then((result) => {
+    
+  })
+  
+  // arrComments.unshift(commentObj);
+  // displayComments();
 });
 
